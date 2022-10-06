8981e355ece77eab750e641d8f16d4d54dc86bd9
Bouman <ndjbouman@gmail.com>
2022-10-06 17:31:47 +0200

    menu slidant

--- assets/js/menu/menu.js
+++ assets/js/menu/menu.js
@@ -17,3 +17,21 @@ menuDiv.appendChild(menuSpan1);
 
 const menuSpan2 = document.createElement("span");
 menuDiv.appendChild(menuSpan2);
+
+
+const navUlPort = document.createElement("ul");
+navUlPort.classList.add("nav-ul-port");
+menuPort.appendChild(navUlPort);
+
+for (let i = 0; i < sectionArray.length; i++) {
+  const navLiPort = document.createElement("li");
+  navLiPort.classList.add("nav-li-port");
+  navUlPort.appendChild(navLiPort);
+
+  const navAPort = document.createElement("a");
+  navAPort.innerHTML = `${sectionArray[i].title}`;
+  navAPort.href = `#${sectionArray[i].title}`;
+  navAPort.classList.add("nav-a-port");
+  navUlPort.appendChild(navAPort);
+}
+
